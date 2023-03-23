cf706ab66a920d464e533e765e31be7b9627c01b
Ryan Liptak <squeek502@hotmail.com>
2023-03-22 23:53:21 -0700

    main: Overhaul arg parsing and add no-preprocess option
    - Args can now use `--`, `-`, or `/` prefixes (this is how llvm-rc works too, win32 rc doesn't support `--`)
    - Support for lack of a space between option and its value (e.g. `/Iinc` will add `inc` as an include dir)
    
    Not fully happy with this (mostly how the no-preprocess is acted on), still needs some work

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const removeComments = @import("comments.zig").removeComments;
+const ParseLineCommandsResult = @import("source_mapping.zig").ParseLineCommandsResult;
 const parseAndRemoveLineCommands = @import("source_mapping.zig").parseAndRemoveLineCommands;
+const SourceMappings = @import("source_mapping.zig").SourceMappings;
 const compile = @import("compile.zig").compile;
 const Diagnostics = @import("errors.zig").Diagnostics;
 
@@ -17,25 +19,60 @@ pub fn main() !void {
     var extra_include_paths = std.ArrayList([]const u8).init(allocator);
     defer extra_include_paths.deinit();
     var ignore_include_env_var = false;
+    var preprocess = true;
 
+    const Arg = struct {
+        prefix: enum { long, short, slash },
+        name: []const u8,
+
+        pub fn fromString(str: []const u8) ?@This() {
+            if (std.mem.startsWith(u8, str, "--")) {
+                return .{ .prefix = .long, .name = str[2..] };
+            } else if (std.mem.startsWith(u8, str, "-")) {
+                return .{ .prefix = .short, .name = str[1..] };
+            } else if (std.mem.startsWith(u8, str, "/")) {
+                return .{ .prefix = .slash, .name = str[1..] };
+            }
+            return null;
+        }
+    };
     var arg_i: usize = 1; // start at 1 to skip past the exe name
     while (arg_i < args.len) {
-        if (std.ascii.eqlIgnoreCase("/I", args[arg_i])) {
-            if (arg_i + 1 >= args.len) {
+        const arg = Arg.fromString(args[arg_i]) orelse break;
+        // -- on its own ends arg parsing
+        if (arg.name.len == 0 and arg.prefix == .long) {
+            arg_i += 1;
+            break;
+        }
+
+        if (std.ascii.startsWithIgnoreCase(arg.name, "i")) {
+            const rest = arg.name[1..];
+            if (rest.len == 0 and arg_i + 1 >= args.len) {
                 std.debug.print("Missing include path after {s} option\n", .{args[arg_i]});
                 std.os.exit(1);
             }
-            try extra_include_paths.append(args[arg_i + 1]);
-            arg_i += 2;
-        } else if (std.ascii.eqlIgnoreCase("/fo", args[arg_i])) {
-            if (arg_i + 1 >= args.len) {
+            try extra_include_paths.append(switch (rest.len) {
+                0 => args[arg_i + 1],
+                else => rest,
+            });
+            arg_i += if (rest.len == 0) 2 else 1;
+        } else if (std.ascii.startsWithIgnoreCase(arg.name, "fo")) {
+            const rest = arg.name[2..];
+            if (rest.len == 0 and arg_i + 1 >= args.len) {
                 std.debug.print("Missing output path after {s} option\n", .{args[arg_i]});
                 std.os.exit(1);
             }
-            output_filename = args[arg_i + 1];
-            arg_i += 2;
-        } else if (std.ascii.eqlIgnoreCase("/x", args[arg_i])) {
+            output_filename = switch (rest.len) {
+                0 => args[arg_i + 1],
+                else => rest,
+            };
+            arg_i += if (rest.len == 0) 2 else 1;
+        } else if (std.ascii.eqlIgnoreCase("x", arg.name)) {
             ignore_include_env_var = true;
+            arg_i += 1;
+        } else if (std.ascii.eqlIgnoreCase("no-preprocess", arg.name)) {
+            preprocess = false;
+            arg_i += 1;
         } else {
             break;
         }
@@ -64,69 +101,85 @@ pub fn main() !void {
         output_filename = output_filename_buf[0..filename_writer.context.pos];
     }
 
-    var argv = std.ArrayList([]const u8).init(allocator);
-    defer argv.deinit();
+    // TODO: I'm getting lost with where the SourceMappings memory lives and result location stuff.
+    //       There's got to be a better way!
+    //       Plan: - Make parseAndRemoveLineCommands take a SourceMappings ptr to insert into
+    //             - Create a SourceMappings on the stack here and populate it when
+    //               the preprocessor is turned off too, since we still want to know
+    //               the line mappings when multiline comments are removed from the input.
+    var input: Input = undefined;
+    if (preprocess) {
+        var argv = std.ArrayList([]const u8).init(allocator);
+        defer argv.deinit();
 
-    try argv.appendSlice(&[_][]const u8{
-        "clang",
-        "-E", // preprocessor only
-        "--comments",
-        "-fuse-line-directives", // #line <num> instead of # <num>
-        // TODO: could use --trace-includes to give info about what's included from where
-        "-xc", // output c
-        // TODO: Turn this off, check the warnings, and convert the spaces back to NUL
-        "-Werror=null-character", // error on null characters instead of converting them to spaces
-        // TODO: could remove -Werror=null-character and instead parse warnings looking for 'warning: null character ignored'
-        //       since the only real problem is when clang doesn't preserve null characters
-        //"-Werror=invalid-pp-token", // will error on unfinished string literals
-        // TODO: could use -Werror instead
-        // https://learn.microsoft.com/en-us/windows/win32/menurc/predefined-macros
-        "-DRC_INVOKED",
-    });
-    for (extra_include_paths.items) |extra_include_path| {
-        try argv.append("--include-directory");
-        try argv.append(extra_include_path);
-    }
-    try argv.append(input_filename);
+        try argv.appendSlice(&[_][]const u8{
+            "clang",
+            "-E", // preprocessor only
+            "--comments",
+            "-fuse-line-directives", // #line <num> instead of # <num>
+            // TODO: could use --trace-includes to give info about what's included from where
+            "-xc", // output c
+            // TODO: Turn this off, check the warnings, and convert the spaces back to NUL
+            "-Werror=null-character", // error on null characters instead of converting them to spaces
+            // TODO: could remove -Werror=null-character and instead parse warnings looking for 'warning: null character ignored'
+            //       since the only real problem is when clang doesn't preserve null characters
+            //"-Werror=invalid-pp-token", // will error on unfinished string literals
+            // TODO: could use -Werror instead
+            // https://learn.microsoft.com/en-us/windows/win32/menurc/predefined-macros
+            "-DRC_INVOKED",
+        });
+        for (extra_include_paths.items) |extra_include_path| {
+            try argv.append("--include-directory");
+            try argv.append(extra_include_path);
+        }
+        try argv.append(input_filename);
 
-    var result = try std.ChildProcess.exec(.{
-        .allocator = allocator,
-        .argv = argv.items,
-        .max_output_bytes = std.math.maxInt(u32),
-    });
-    defer allocator.free(result.stdout);
-    defer allocator.free(result.stderr);
+        var result = try std.ChildProcess.exec(.{
+            .allocator = allocator,
+            .argv = argv.items,
+            .max_output_bytes = std.math.maxInt(u32),
+        });
+        errdefer allocator.free(result.stdout);
+        defer allocator.free(result.stderr);
 
-    switch (result.term) {
-        .Exited => |code| {
-            if (code != 0) {
-                // TODO: Better formatting
-                std.debug.print("Preprocessor errors:\n{s}\n", .{result.stderr});
-                return error.ExitCodeFailure;
-            }
-        },
-        .Signal, .Stopped, .Unknown => {
-            return error.ClangProcessTerminated;
-        },
-    }
+        switch (result.term) {
+            .Exited => |code| {
+                if (code != 0) {
+                    // TODO: Better formatting
+                    std.debug.print("Preprocessor errors:\n{s}\n", .{result.stderr});
+                    return error.ExitCodeFailure;
+                }
+            },
+            .Signal, .Stopped, .Unknown => {
+                return error.ClangProcessTerminated;
+            },
+        }
 
-    var mapping_results = try parseAndRemoveLineCommands(allocator, result.stdout, result.stdout);
-    defer mapping_results.mappings.deinit(allocator);
+        input.buf = result.stdout;
+        input.result = .{ .preprocessed = try parseAndRemoveLineCommands(allocator, input.buf, input.buf) };
+        errdefer input.result.preprocessed.mappings.deinit(allocator);
 
-    // Set the root file
-    if (mapping_results.mappings.files.getOffset(input_filename)) |root_filename_offset| {
-        mapping_results.mappings.root_filename_offset = root_filename_offset;
+        // Set the root file
+        if (input.result.preprocessed.mappings.files.getOffset(input_filename)) |root_filename_offset| {
+            input.result.preprocessed.mappings.root_filename_offset = root_filename_offset;
+        } else {
+            // This *should* be impossible, as the clang preprocessor inserts whatever filename
+            // you give it into the #line directives (e.g. `.\./rCDaTA.rC` for a file called
+            // `rcdata.rc` will get a `#line 1 ".\\./rCDaTA.rC"` directive), but this may still
+            // happen if there is a mismatch in how the line directive strings are parsed versus
+            // how they are escaped/written by the preprocessor.
+            std.debug.print("input filename not found in source mappings: {s}\n", .{input_filename});
+            @panic("Internal error (this is a bug)");
+        }
     } else {
-        // This *should* be impossible, as the clang preprocessor inserts whatever filename
-        // you give it into the #line directives (e.g. `.\./rCDaTA.rC` for a file called
-        // `rcdata.rc` will get a `#line 1 ".\\./rCDaTA.rC"` directive), but this may still
-        // happen if there is a mismatch in how the line directive strings are parsed versus
-        // how they are escaped/written by the preprocessor.
-        std.debug.print("input filename not found in source mappings: {s}\n", .{input_filename});
-        @panic("Internal error (this is a bug)");
-    }
+        input.buf = try std.fs.cwd().readFileAlloc(allocator, input_filename, std.math.maxInt(usize));
+        errdefer allocator.free(input.buf);
 
-    var preprocessed_input = removeComments(mapping_results.result, mapping_results.result, &mapping_results.mappings);
+        input.result = .{ .raw = input.buf };
+    }
+    defer input.deinit(allocator);
+
+    var final_input = removeComments(input.resultSlice(), input.resultSlice(), input.mappingsPtr());
 
     var output_file = try std.fs.cwd().createFile(output_filename.?, .{});
     defer output_file.close();
@@ -134,28 +187,70 @@ pub fn main() !void {
     var diagnostics = Diagnostics.init(allocator);
     defer diagnostics.deinit();
 
-    // std.debug.print("after preprocessor:\n------------------\n{s}\n------------------\n\nmappings:\n", .{preprocessed_input});
-    // for (mapping_results.mappings.mapping.items) |span, i| {
-    //     const line_num = i + 1;
-    //     const filename = mapping_results.mappings.files.get(span.filename_offset);
-    //     std.debug.print("{}: {s}:{}-{}\n", .{ line_num, filename, span.start_line, span.end_line });
+    // std.debug.print("after preprocessor:\n------------------\n{s}\n------------------\n", .{final_input});
+    // if (input.mappingsPtr()) |mappings| {
+    //     std.debug.print("\nmappings:\n", .{});
+    //     for (mappings.mapping.items, 0..) |span, i| {
+    //         const line_num = i + 1;
+    //         const filename = mappings.files.get(span.filename_offset);
+    //         std.debug.print("{}: {s}:{}-{}\n", .{ line_num, filename, span.start_line, span.end_line });
+    //     }
     // }
     // std.debug.print("\n", .{});
 
-    compile(allocator, preprocessed_input, output_file.writer(), .{
+    compile(allocator, final_input, output_file.writer(), .{
         .cwd = std.fs.cwd(),
         .diagnostics = &diagnostics,
-        .source_mappings = &mapping_results.mappings,
+        .source_mappings = input.mappingsPtr(),
         .ignore_include_env_var = ignore_include_env_var,
         .extra_include_paths = extra_include_paths.items,
     }) catch |err| switch (err) {
         error.ParseError, error.CompileError => {
-            diagnostics.renderToStdErr(std.fs.cwd(), preprocessed_input, mapping_results.mappings);
+            diagnostics.renderToStdErr(std.fs.cwd(), final_input, input.mappings());
             std.os.exit(1);
         },
         else => |e| return e,
     };
 
     // print any warnings/notes
-    diagnostics.renderToStdErr(std.fs.cwd(), preprocessed_input, mapping_results.mappings);
+    diagnostics.renderToStdErr(std.fs.cwd(), final_input, input.mappings());
 }
+
+const Input = struct {
+    buf: []u8,
+    result: union(enum) {
+        raw: []u8,
+        preprocessed: ParseLineCommandsResult,
+    },
+
+    pub fn resultSlice(self: *Input) []u8 {
+        return switch (self.result) {
+            .raw => |s| s,
+            .preprocessed => |r| r.result,
+        };
+    }
+
+    pub fn mappingsPtr(self: *Input) ?*SourceMappings {
+        return switch (self.result) {
+            .raw => null,
+            .preprocessed => |*r| return &r.mappings,
+        };
+    }
+
+    pub fn mappings(self: *Input) ?SourceMappings {
+        return switch (self.result) {
+            .raw => null,
+            .preprocessed => |r| return r.mappings,
+        };
+    }
+
+    pub fn deinit(self: *Input, allocator: std.mem.Allocator) void {
+        allocator.free(self.buf);
+        switch (self.result) {
+            .preprocessed => |*preprocessed| {
+                preprocessed.mappings.deinit(allocator);
+            },
+            .raw => {},
+        }
+    }
+};
