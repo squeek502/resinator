0386e0cf8d6e0671d1241c4f5a7811d06fd706e2
Ryan Liptak <squeek502@hotmail.com>
2023-03-23 21:02:35 -0700

    Move arg parsing to separate file
    This is mostly prep for some other forthcoming changes

---
+++ src/cli.zig
@@ -0,0 +1,181 @@
+const std = @import("std");
+const CodePage = @import("code_pages.zig").CodePage;
+const lang = @import("lang.zig");
+
+pub const Options = struct {
+    allocator: std.mem.Allocator,
+    input_filename: []const u8 = &[_]u8{},
+    output_filename: []const u8 = &[_]u8{},
+    extra_include_paths: std.ArrayListUnmanaged([]const u8) = .{},
+    ignore_include_env_var: bool = false,
+    preprocess: bool = true,
+    default_language_id: ?u16 = null,
+    default_code_page: ?CodePage = null,
+    verbose: bool = false,
+
+    pub fn deinit(self: *Options) void {
+        self.extra_include_paths.deinit(self.allocator);
+        self.allocator.free(self.input_filename);
+        self.allocator.free(self.output_filename);
+    }
+};
+
+pub const Arg = struct {
+    prefix: enum { long, short, slash },
+    name: []const u8,
+
+    pub fn fromString(str: []const u8) ?@This() {
+        if (std.mem.startsWith(u8, str, "--")) {
+            return .{ .prefix = .long, .name = str[2..] };
+        } else if (std.mem.startsWith(u8, str, "-")) {
+            return .{ .prefix = .short, .name = str[1..] };
+        } else if (std.mem.startsWith(u8, str, "/")) {
+            return .{ .prefix = .slash, .name = str[1..] };
+        }
+        return null;
+    }
+};
+
+pub fn parse(allocator: std.mem.Allocator, args: []const []const u8) !Options {
+    var options = Options{ .allocator = allocator };
+    errdefer options.deinit();
+
+    var output_filename: ?[]const u8 = null;
+
+    var arg_i: usize = 1; // start at 1 to skip past the exe name
+    while (arg_i < args.len) {
+        const arg = Arg.fromString(args[arg_i]) orelse break;
+        // -- on its own ends arg parsing
+        if (arg.name.len == 0 and arg.prefix == .long) {
+            arg_i += 1;
+            break;
+        }
+
+        if (std.ascii.startsWithIgnoreCase(arg.name, "i")) {
+            const rest = arg.name[1..];
+            if (rest.len == 0 and arg_i + 1 >= args.len) {
+                std.debug.print("Missing include path after {s} option\n", .{args[arg_i]});
+                std.os.exit(1);
+            }
+            try options.extra_include_paths.append(options.allocator, switch (rest.len) {
+                0 => args[arg_i + 1],
+                else => rest,
+            });
+            arg_i += if (rest.len == 0) 2 else 1;
+        } else if (std.ascii.startsWithIgnoreCase(arg.name, "fo")) {
+            const rest = arg.name[2..];
+            if (rest.len == 0 and arg_i + 1 >= args.len) {
+                std.debug.print("Missing output path after {s} option\n", .{args[arg_i]});
+                std.os.exit(1);
+            }
+            output_filename = switch (rest.len) {
+                0 => args[arg_i + 1],
+                else => rest,
+            };
+            arg_i += if (rest.len == 0) 2 else 1;
+        }
+        // Note: This must come before the "l" case to avoid becoming a dead branch
+        else if (std.ascii.startsWithIgnoreCase(arg.name, "ln")) {
+            const rest = arg.name[2..];
+            if (rest.len == 0 and arg_i + 1 >= args.len) {
+                std.debug.print("Missing language tag after {s} option\n", .{args[arg_i]});
+                std.os.exit(1);
+            }
+            const tag = switch (rest.len) {
+                0 => args[arg_i + 1],
+                else => rest,
+            };
+            options.default_language_id = lang.tagToInt(tag) catch {
+                std.debug.print("Invalid language tag: {s}\n", .{tag});
+                std.os.exit(1);
+            };
+            if (options.default_language_id.? == lang.LOCALE_CUSTOM_UNSPECIFIED) {
+                // TODO: Better formatting
+                std.debug.print("Warning: language tag '{s}' does not have an assigned ID so it has been converted to LOCALE_CUSTOM_UNSPECIFIED (id=0x{x})\n", .{ tag, lang.LOCALE_CUSTOM_UNSPECIFIED });
+            }
+            arg_i += if (rest.len == 0) 2 else 1;
+        } else if (std.ascii.startsWithIgnoreCase(arg.name, "l")) {
+            const rest = arg.name[1..];
+            if (rest.len == 0 and arg_i + 1 >= args.len) {
+                std.debug.print("Missing language ID after {s} option\n", .{args[arg_i]});
+                std.os.exit(1);
+            }
+            const num_str = switch (rest.len) {
+                0 => args[arg_i + 1],
+                else => rest,
+            };
+            options.default_language_id = lang.parseInt(num_str) catch {
+                std.debug.print("Invalid language ID: {s}\n", .{num_str});
+                std.os.exit(1);
+            };
+            arg_i += if (rest.len == 0) 2 else 1;
+        } else if (std.ascii.startsWithIgnoreCase(arg.name, "c")) {
+            const rest = arg.name[1..];
+            if (rest.len == 0 and arg_i + 1 >= args.len) {
+                std.debug.print("Missing code page ID after {s} option\n", .{args[arg_i]});
+                std.os.exit(1);
+            }
+            const num_str = switch (rest.len) {
+                0 => args[arg_i + 1],
+                else => rest,
+            };
+            const code_page_id = std.fmt.parseUnsigned(u16, num_str, 10) catch {
+                std.debug.print("Invalid code page ID: {s}\n", .{num_str});
+                std.os.exit(1);
+            };
+            options.default_code_page = CodePage.getByIdentifierEnsureSupported(code_page_id) catch |err| switch (err) {
+                error.InvalidCodePage => {
+                    std.debug.print("Invalid or unknown code page ID: {}\n", .{code_page_id});
+                    std.os.exit(1);
+                },
+                error.UnsupportedCodePage => {
+                    std.debug.print("Unsupported code page: {s} (id={})\n", .{
+                        @tagName(CodePage.getByIdentifier(code_page_id) catch unreachable),
+                        code_page_id,
+                    });
+                    std.os.exit(1);
+                },
+            };
+            arg_i += if (rest.len == 0) 2 else 1;
+        } else if (std.ascii.eqlIgnoreCase("v", arg.name)) {
+            options.verbose = true;
+            arg_i += 1;
+        } else if (std.ascii.eqlIgnoreCase("x", arg.name)) {
+            options.ignore_include_env_var = true;
+            arg_i += 1;
+        } else if (std.ascii.eqlIgnoreCase("no-preprocess", arg.name)) {
+            options.preprocess = false;
+            arg_i += 1;
+        } else {
+            break;
+        }
+    }
+
+    var positionals = args[arg_i..];
+
+    if (positionals.len < 1) {
+        std.debug.print("Missing input filename\n", .{});
+        std.os.exit(1);
+    }
+    options.input_filename = try allocator.dupe(u8, positionals[0]);
+
+    if (positionals.len > 1) {
+        if (output_filename != null) {
+            std.debug.print("Output filename already specified with the /fo option\n", .{});
+            std.os.exit(1);
+        }
+        output_filename = positionals[1];
+    }
+    if (output_filename == null) {
+        var output_filename_buf: [std.fs.MAX_NAME_BYTES]u8 = undefined;
+        var filename_fbs = std.io.fixedBufferStream(&output_filename_buf);
+        var filename_writer = filename_fbs.writer();
+        try filename_writer.writeAll(std.fs.path.stem(options.input_filename));
+        try filename_writer.writeAll(".res");
+        output_filename = output_filename_buf[0..filename_writer.context.pos];
+    }
+
+    options.output_filename = try allocator.dupe(u8, output_filename.?);
+
+    return options;
+}
--- src/main.zig
+++ src/main.zig
@@ -1,180 +1,25 @@
 const std = @import("std");
 const removeComments = @import("comments.zig").removeComments;
-const ParseLineCommandsResult = @import("source_mapping.zig").ParseLineCommandsResult;
 const parseAndRemoveLineCommands = @import("source_mapping.zig").parseAndRemoveLineCommands;
-const SourceMappings = @import("source_mapping.zig").SourceMappings;
 const compile = @import("compile.zig").compile;
 const Diagnostics = @import("errors.zig").Diagnostics;
-const lang = @import("lang.zig");
-const CodePage = @import("code_pages.zig").CodePage;
+const cli = @import("cli.zig");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{ .stack_trace_frames = 8 }){};
     defer std.debug.assert(gpa.deinit() == false);
     const allocator = gpa.allocator();
 
-    var args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
+    var options = options: {
+        var args = try std.process.argsAlloc(allocator);
+        defer std.process.argsFree(allocator, args);
 
-    var output_filename: ?[]const u8 = null;
-    var output_filename_buf: [std.fs.MAX_NAME_BYTES]u8 = undefined;
-    var extra_include_paths = std.ArrayList([]const u8).init(allocator);
-    defer extra_include_paths.deinit();
-    var ignore_include_env_var = false;
-    var preprocess = true;
-    var default_language_id: ?u16 = null;
-    var default_code_page: ?CodePage = null;
-    var verbose = false;
-
-    const Arg = struct {
-        prefix: enum { long, short, slash },
-        name: []const u8,
-
-        pub fn fromString(str: []const u8) ?@This() {
-            if (std.mem.startsWith(u8, str, "--")) {
-                return .{ .prefix = .long, .name = str[2..] };
-            } else if (std.mem.startsWith(u8, str, "-")) {
-                return .{ .prefix = .short, .name = str[1..] };
-            } else if (std.mem.startsWith(u8, str, "/")) {
-                return .{ .prefix = .slash, .name = str[1..] };
-            }
-            return null;
-        }
+        break :options try cli.parse(allocator, args);
     };
-    var arg_i: usize = 1; // start at 1 to skip past the exe name
-    while (arg_i < args.len) {
-        const arg = Arg.fromString(args[arg_i]) orelse break;
-        // -- on its own ends arg parsing
-        if (arg.name.len == 0 and arg.prefix == .long) {
-            arg_i += 1;
-            break;
-        }
-
-        if (std.ascii.startsWithIgnoreCase(arg.name, "i")) {
-            const rest = arg.name[1..];
-            if (rest.len == 0 and arg_i + 1 >= args.len) {
-                std.debug.print("Missing include path after {s} option\n", .{args[arg_i]});
-                std.os.exit(1);
-            }
-            try extra_include_paths.append(switch (rest.len) {
-                0 => args[arg_i + 1],
-                else => rest,
-            });
-            arg_i += if (rest.len == 0) 2 else 1;
-        } else if (std.ascii.startsWithIgnoreCase(arg.name, "fo")) {
-            const rest = arg.name[2..];
-            if (rest.len == 0 and arg_i + 1 >= args.len) {
-                std.debug.print("Missing output path after {s} option\n", .{args[arg_i]});
-                std.os.exit(1);
-            }
-            output_filename = switch (rest.len) {
-                0 => args[arg_i + 1],
-                else => rest,
-            };
-            arg_i += if (rest.len == 0) 2 else 1;
-        }
-        // Note: This must come before the "l" case to avoid becoming a dead branch
-        else if (std.ascii.startsWithIgnoreCase(arg.name, "ln")) {
-            const rest = arg.name[2..];
-            if (rest.len == 0 and arg_i + 1 >= args.len) {
-                std.debug.print("Missing language tag after {s} option\n", .{args[arg_i]});
-                std.os.exit(1);
-            }
-            const tag = switch (rest.len) {
-                0 => args[arg_i + 1],
-                else => rest,
-            };
-            default_language_id = lang.tagToInt(tag) catch {
-                std.debug.print("Invalid language tag: {s}\n", .{tag});
-                std.os.exit(1);
-            };
-            if (default_language_id.? == lang.LOCALE_CUSTOM_UNSPECIFIED) {
-                // TODO: Better formatting
-                std.debug.print("Warning: language tag '{s}' does not have an assigned ID so it has been converted to LOCALE_CUSTOM_UNSPECIFIED (id=0x{x})\n", .{ tag, lang.LOCALE_CUSTOM_UNSPECIFIED });
-            }
-            arg_i += if (rest.len == 0) 2 else 1;
-        } else if (std.ascii.startsWithIgnoreCase(arg.name, "l")) {
-            const rest = arg.name[1..];
-            if (rest.len == 0 and arg_i + 1 >= args.len) {
-                std.debug.print("Missing language ID after {s} option\n", .{args[arg_i]});
-                std.os.exit(1);
-            }
-            const num_str = switch (rest.len) {
-                0 => args[arg_i + 1],
-                else => rest,
-            };
-            default_language_id = lang.parseInt(num_str) catch {
-                std.debug.print("Invalid language ID: {s}\n", .{num_str});
-                std.os.exit(1);
-            };
-            arg_i += if (rest.len == 0) 2 else 1;
-        } else if (std.ascii.startsWithIgnoreCase(arg.name, "c")) {
-            const rest = arg.name[1..];
-            if (rest.len == 0 and arg_i + 1 >= args.len) {
-                std.debug.print("Missing code page ID after {s} option\n", .{args[arg_i]});
-                std.os.exit(1);
-            }
-            const num_str = switch (rest.len) {
-                0 => args[arg_i + 1],
-                else => rest,
-            };
-            const code_page_id = std.fmt.parseUnsigned(u16, num_str, 10) catch {
-                std.debug.print("Invalid code page ID: {s}\n", .{num_str});
-                std.os.exit(1);
-            };
-            default_code_page = CodePage.getByIdentifierEnsureSupported(code_page_id) catch |err| switch (err) {
-                error.InvalidCodePage => {
-                    std.debug.print("Invalid or unknown code page ID: {}\n", .{code_page_id});
-                    std.os.exit(1);
-                },
-                error.UnsupportedCodePage => {
-                    std.debug.print("Unsupported code page: {s} (id={})\n", .{
-                        @tagName(CodePage.getByIdentifier(code_page_id) catch unreachable),
-                        code_page_id,
-                    });
-                    std.os.exit(1);
-                },
-            };
-            arg_i += if (rest.len == 0) 2 else 1;
-        } else if (std.ascii.eqlIgnoreCase("v", arg.name)) {
-            verbose = true;
-            arg_i += 1;
-        } else if (std.ascii.eqlIgnoreCase("x", arg.name)) {
-            ignore_include_env_var = true;
-            arg_i += 1;
-        } else if (std.ascii.eqlIgnoreCase("no-preprocess", arg.name)) {
-            preprocess = false;
-            arg_i += 1;
-        } else {
-            break;
-        }
-    }
-
-    var positionals = args[arg_i..];
-
-    if (positionals.len < 1) {
-        std.debug.print("Missing input filename\n", .{});
-        std.os.exit(1);
-    }
-    const input_filename = positionals[0];
-
-    if (positionals.len > 1) {
-        if (output_filename != null) {
-            std.debug.print("Output filename already specified with the /fo option\n", .{});
-            std.os.exit(1);
-        }
-        output_filename = positionals[1];
-    }
-    if (output_filename == null) {
-        var filename_fbs = std.io.fixedBufferStream(&output_filename_buf);
-        var filename_writer = filename_fbs.writer();
-        try filename_writer.writeAll(std.fs.path.stem(input_filename));
-        try filename_writer.writeAll(".res");
-        output_filename = output_filename_buf[0..filename_writer.context.pos];
-    }
+    defer options.deinit();
 
     var full_input = full_input: {
-        if (preprocess) {
+        if (options.preprocess) {
             var argv = std.ArrayList([]const u8).init(allocator);
             defer argv.deinit();
 
@@ -194,11 +39,11 @@ pub fn main() !void {
                 // https://learn.microsoft.com/en-us/windows/win32/menurc/predefined-macros
                 "-DRC_INVOKED",
             });
-            for (extra_include_paths.items) |extra_include_path| {
+            for (options.extra_include_paths.items) |extra_include_path| {
                 try argv.append("--include-directory");
                 try argv.append(extra_include_path);
             }
-            try argv.append(input_filename);
+            try argv.append(options.input_filename);
 
             var result = try std.ChildProcess.exec(.{
                 .allocator = allocator,
@@ -223,7 +68,7 @@ pub fn main() !void {
 
             break :full_input result.stdout;
         } else {
-            break :full_input try std.fs.cwd().readFileAlloc(allocator, input_filename, std.math.maxInt(usize));
+            break :full_input try std.fs.cwd().readFileAlloc(allocator, options.input_filename, std.math.maxInt(usize));
         }
     };
     defer allocator.free(full_input);
@@ -231,11 +76,11 @@ pub fn main() !void {
     // Note: We still want to run this when no-preprocess is set because:
     //   1. We want to print accurate line numbers after removing multiline comments
     //   2. We want to be able to handle an already-preprocessed input with #line commands in it
-    var mapping_results = try parseAndRemoveLineCommands(allocator, full_input, full_input, .{ .initial_filename = input_filename });
+    var mapping_results = try parseAndRemoveLineCommands(allocator, full_input, full_input, .{ .initial_filename = options.input_filename });
     defer mapping_results.mappings.deinit(allocator);
 
     // Set the root file
-    if (mapping_results.mappings.files.getOffset(input_filename)) |root_filename_offset| {
+    if (mapping_results.mappings.files.getOffset(options.input_filename)) |root_filename_offset| {
         mapping_results.mappings.root_filename_offset = root_filename_offset;
     } else {
         // This *should* be impossible, as the clang preprocessor inserts whatever filename
@@ -243,13 +88,13 @@ pub fn main() !void {
         // `rcdata.rc` will get a `#line 1 ".\\./rCDaTA.rC"` directive), but this may still
         // happen if there is a mismatch in how the line directive strings are parsed versus
         // how they are escaped/written by the preprocessor.
-        std.debug.print("input filename not found in source mappings: {s}\n", .{input_filename});
+        std.debug.print("input filename not found in source mappings: {s}\n", .{options.input_filename});
         @panic("Internal error (this is a bug)");
     }
 
     var final_input = removeComments(mapping_results.result, mapping_results.result, &mapping_results.mappings);
 
-    var output_file = try std.fs.cwd().createFile(output_filename.?, .{});
+    var output_file = try std.fs.cwd().createFile(options.output_filename, .{});
     defer output_file.close();
 
     var diagnostics = Diagnostics.init(allocator);
@@ -268,11 +113,11 @@ pub fn main() !void {
         .cwd = std.fs.cwd(),
         .diagnostics = &diagnostics,
         .source_mappings = &mapping_results.mappings,
-        .ignore_include_env_var = ignore_include_env_var,
-        .extra_include_paths = extra_include_paths.items,
-        .default_language_id = default_language_id,
-        .default_code_page = default_code_page orelse .windows1252,
-        .verbose = verbose,
+        .ignore_include_env_var = options.ignore_include_env_var,
+        .extra_include_paths = options.extra_include_paths.items,
+        .default_language_id = options.default_language_id,
+        .default_code_page = options.default_code_page orelse .windows1252,
+        .verbose = options.verbose,
     }) catch |err| switch (err) {
         error.ParseError, error.CompileError => {
             diagnostics.renderToStdErr(std.fs.cwd(), final_input, mapping_results.mappings);
