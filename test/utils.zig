4195d11f54ae58e72020d9bc8ab5b9ad18236fbd
Ryan Liptak <squeek502@hotmail.com>
2026-06-29 19:20:09 -0700

    Update section_number usage for latest Zig

--- test/utils.zig
+++ test/utils.zig
@@ -494,9 +494,9 @@ pub fn stripAndFixupCoff(allocator: Allocator, reader: *std.Io.Reader, writer: *
                 try reader.discardAll(std.coff.Symbol.sizeOf());
             }
         } else {
-            const section_number: std.coff.SectionNumber = @enumFromInt(std.mem.readInt(u16, symbol_bytes[12..14], .little));
+            const section_number: std.coff.SectionNumber = @enumFromInt(std.mem.readInt(i16, symbol_bytes[12..14], .little));
             if (name[0] != '@' and section_number != .UNDEFINED and section_number != .ABSOLUTE and section_number != .DEBUG) {
-                std.mem.writeInt(u16, symbol_bytes[12..14], @intFromEnum(section_number) - stripped_sections, .little);
+                std.mem.writeInt(u16, symbol_bytes[12..14], @as(u16, @intCast(@intFromEnum(section_number))) - stripped_sections, .little);
             }
             try writer.writeAll(symbol_bytes);
             std.debug.assert(number_of_aux_symbols <= 1);
